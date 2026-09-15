e1fbb38140b105acd3e5deb9213a314da8b5aa39
Deepanshu Rawat <rawat.deepanshu9968@gmail.com>
2026-09-15 23:54:39 +0530

    change the .env secret name of Frontend and backend URL

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ import orderRoutes from "./routes/orderRoutes";
 
 const app = express();
 
-const CLIENT_URL = process.env.CLIENT_API_URL;
+const CLIENT_URL = process.env.CLIENT_URL;
 
 if (!CLIENT_URL) {
   throw new Error("CLIENT_URL is not defined");
