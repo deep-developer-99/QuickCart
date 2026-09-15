3cf83cad39ff52011b398fe0516c9be3b7cc5f42
Deepanshu Rawat <rawat.deepanshu9968@gmail.com>
2026-09-15 22:41:09 +0530

    Change the CORS code in backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,9 +11,15 @@ import orderRoutes from "./routes/orderRoutes";
 
 const app = express();
 
+const CLIENT_URL = process.env.CLIENT_URL;
+
+if (!CLIENT_URL) {
+  throw new Error("CLIENT_URL is not defined");
+}
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: CLIENT_URL,
     credentials: true,
   }),
 );
