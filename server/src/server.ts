8761852311d92e8ab0ba99fb698753df2a3670c3
Deepanshu Rawat <rawat.deepanshu9968@gmail.com>
2026-09-16 15:40:38 +0530

    solve the problem of Server host binding

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,12 +4,12 @@ import connectDB from "./config/db";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 const startServer = async (): Promise<void> => {
   await connectDB();
 
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
 };
